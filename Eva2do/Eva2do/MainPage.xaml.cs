29c5fb9412bdc1f39f19219b7a44ae0b2db9c894
Miguel Ángel Delgado Grande <miguelangeldelgadog18.cb3@dgeti.sems.gob.mx>
2021-04-24 12:47:00 -0500

    Agregar archivos de proyecto.

---
+++ Eva2do/Eva2do/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Eva2do
+{
+    public partial class MainPage : MasterDetailPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            this.Master = new master();
+            this.Detail = new NavigationPage(new detail());
+
+            App.MasterD = this;
+        }
+    }
+}
---
+++ Eva2do/Eva2do/master.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Eva2do
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class master : ContentPage
+    {
+        public master()
+        {
+            InitializeComponent();
+        }
+
+        private async void bp1_Clicked(object sender, EventArgs e)
+        {
+            App.MasterD.IsPresented = false;
+            await App.MasterD.Detail.Navigation.PushAsync(new P1());
+        }
+
+        private async void bp2_Clicked(object sender, EventArgs e)
+        {
+            App.MasterD.IsPresented = false;
+            await App.MasterD.Detail.Navigation.PushAsync(new P2());
+        }
+
+        private async void bp3_Clicked(object sender, EventArgs e)
+        {
+            App.MasterD.IsPresented = false;
+            await App.MasterD.Detail.Navigation.PushAsync(new P3());
+        }
+
+        private async void bp4_Clicked(object sender, EventArgs e)
+        {
+            App.MasterD.IsPresented = false;
+            await App.MasterD.Detail.Navigation.PushAsync(new P4());
+        }
+    }
+}
